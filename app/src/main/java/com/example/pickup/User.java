fbc49a30b88104be1520ff1b9e63810889bdb0f5
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2020-02-15 09:11:38 -0500

    Added skeleton for ExpressIntent and destroyEvent in class user. ExpressIntent depends on class Event so currently there is a function not found error.

--- app/src/main/java/com/example/pickup/User.java
+++ app/src/main/java/com/example/pickup/User.java
@@ -14,7 +14,7 @@ public class User {
     /**
      * Create an event hosted by the user.
      */
-    void create_event() {
+    void createEvent() {
         if (hasActiveEvent) {
             System.out.println("Already have an active game");
             return;
@@ -22,11 +22,27 @@ public class User {
         hasActiveEvent = true;
 
         //Must communicate with server to create geofence
-
         //
-
         //create new event
 
     }
+
+    /**
+     * Destroys the active event of a user.
+     */
+    void destroyEvent() { //Called to event which then calls here
+        hasActiveEvent = false;
+    }
+
+    /**
+     * Expresses to an event that a user would like to join.
+     * @param event the event the user is interested in.
+     */
+    void expressIntent(Event event) {
+        event.addGuestWithIntent(this); //perhaps want to send distance as well?
+        hasActiveEvent = true;
+    }
+
+
 
 }
