b3c211a9b247e5a910569d1d2d5b8bde48f5be61
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2020-02-15 08:23:14 -0500

    Added skeleton for creating a user in main and the corresponding contructor in User. Also added the basic logic for createEvent in User.

--- app/src/main/java/com/example/pickup/MainActivity.java
+++ app/src/main/java/com/example/pickup/MainActivity.java
@@ -11,8 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    String deviceID;
+    String name;
 
     // COMMUNICATE W SERVER FOR USER ID
-    
+
     //
+    User user = new User(deviceID, name);
 }
--- app/src/main/java/com/example/pickup/User.java
+++ app/src/main/java/com/example/pickup/User.java
@@ -1,4 +1,32 @@
 package com.example.pickup;
 
 public class User {
+    private int activeEvents;
+    private String deviceID;
+    private String name;
+
+    public User (String givenID, String givenName) {
+        activeEvents = 0;
+        deviceID = givenID;
+        name = givenName;
+    }
+
+    /**
+     * Create an event hosted by the user.
+     */
+    void create_event() {
+        if (activeEvents != 0) {
+            System.out.println("Already have an active game");
+            return;
+        }
+        activeEvents += 1;
+
+        //Must communicate with server to create geofence
+
+        //
+
+        //create new event
+
+    }
+
 }
