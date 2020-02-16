aeda5131511d7bcbdfd4d52eb1f3002f589f1b04
Mohammad Faaris Ali <faaris.ali@mail.utoronto.ca>
2020-02-16 06:46:58 -0500

    Added main screen into control flow of program.

--- app/src/main/java/com/example/pickup/MainActivity.java
+++ app/src/main/java/com/example/pickup/MainActivity.java
@@ -2,18 +2,41 @@ package com.example.pickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import io.radar.sdk.Radar;
 
 public class MainActivity extends AppCompatActivity {
-
+    public Button goButton;
+    public EditText name;
+    public Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Radar.initialize("prj_live_pk_598126e8bc2d50f1da77cf8261a9255c9c7d63fc");
         Radar.startTracking();
+
+        intent = new Intent(this, MapActivity.class);
+
+        name = findViewById(R.id.nameText);
+        goButton = findViewById(R.id.bStartButton);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    (new PickUpClient()).nameSet(name.getText().toString());
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                    System.out.println("Cannot set name");
+                }
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -21,11 +44,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Radar.stopTracking();
     }
-    String deviceID;
-    String name;
-
-    // COMMUNICATE W SERVER FOR USER ID
-
-    //
-    User user = new User(deviceID, name);
 }
